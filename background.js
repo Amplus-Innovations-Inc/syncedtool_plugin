d92130ad0b11193c7f84206bf971dc0dabea475c
Henry Huang <henry.huang@amplusinnovations.com>
2021-08-11 22:13:00 -0400

    condensed version of previous commit

--- background.js
+++ background.js
@@ -1,6 +1,8 @@
 $(document).ready(function () {
   var link_array = [];
   var email_array = [];
+  var link_finished = 0;
+  var elem = document.getElementById("myBar");
   $("#email_csv").on("change", () => {
     if (document.getElementById("email_csv").value != "") {
       document.getElementById("email-upload-button").style.background = "grey";
@@ -137,15 +139,42 @@ $(document).ready(function () {
 
   $("#submit-file").on("click", (e) => {
     e.preventDefault();
-    var link_finished = 0;
-    var elem = document.getElementById("myBar");
 
     console.log(link_array.length);
-    s = 0;
+    share(link_array[0]).then(() => {
+      setTimeout(() => {
+        for (s = 1; s < link_array.length; s++) {
+          share(link_array[s]);
+        }
+      }, 600);
+    });
 
-    fetchWithTimeout(
+    document.getElementById("submit-file").style.background = "grey";
+    document.getElementById("submit-file").disabled = true;
+  });
+  function validateEmail(email) {
+    const re =
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+    return re.test(String(email).toLowerCase());
+  }
+  async function fetchWithTimeout(resource, options = {}) {
+    const { timeout = 10000 } = options;
+
+    const controller = new AbortController();
+    const id = setTimeout(() => controller.abort(), timeout);
+
+    const response = await fetch(resource, {
+      ...options,
+      signal: controller.signal,
+    });
+    clearTimeout(id);
+
+    return response;
+  }
+  async function share(link) {
+    response = await fetchWithTimeout(
       "https://exocloud.syncedtool.ca/api/2/sharelinks/" +
-        link_array[s] + //change link var
+        link + //change link var
         "?include_subscribers=true"
     )
       .then((res) => res.json())
@@ -245,148 +274,10 @@ $(document).ready(function () {
               "Error sharing :(";
           });
       })
-      .then(() => {
-        for (s = 1; s < link_array.length; s++) {
-          fetchWithTimeout(
-            "https://exocloud.syncedtool.ca/api/2/sharelinks/" +
-              link_array[s] + //change link var
-              "?include_subscribers=true"
-          )
-            .then((res) => res.json())
-            .then((res) => {
-              //   var node = document.createElement("li");
-              //   var textnode = document.createTextNode(res["id"]);
-              //   node.appendChild(textnode);
-              //   document.getElementById("list").appendChild(node);
-              var jsonVariable = {};
-              link_finished++;
-              console.log((link_finished / link_array.length / 2) * 100);
-              elem.style.width =
-                (link_finished / link_array.length / 2) * 100 + "%";
-
-              for (j = 0; j < email_array.length; j++) {
-                //change email var
-                var write_access =
-                  document.getElementById("write_access").checked;
-                var delete_access =
-                  document.getElementById("delete_access").checked;
-                console.log(document.getElementById("write_access").checked);
-                jsonVariable[email_array[j]] = {
-                  account_id: email_array[j],
-                  account_type: "email",
-                  write_access: write_access, //change
-                  delete_access: delete_access, //change
-                };
-              }
-              for (i = 0; i < res.subscribers.length; i++) {
-                if (res.subscribers[i].subscriber_type != "public") {
-                  if (
-                    email_array.includes(res.subscribers[i].subscriber.email) ==
-                    true
-                  ) {
-                    delete jsonVariable[res.subscribers[i].subscriber.email];
-                  }
-                  var account_id = res.subscribers[i].subscriber.id;
-
-                  var account_type = res.subscribers[i].subscriber_type;
-
-                  var write_access = res.subscribers[i].write_access;
-
-                  var delete_access = res.subscribers[i].delete_access;
-
-                  jsonVariable[
-                    account_type + "_" + res.subscribers[i].subscriber.id
-                  ] = {
-                    account_id: account_id,
-                    account_type: account_type,
-                    write_access: write_access,
-                    delete_access: delete_access,
-                  };
-                }
-              }
-              var ele = document.getElementsByName("notify");
-              var notify_recipients = "new";
-              for (i = 0; i < ele.length; i++) {
-                if (ele[i].checked) {
-                  notify_recipients = ele[i].value;
-                }
-              }
-              console.log("index " + link_finished);
-
-              fetchWithTimeout(
-                "https://exocloud.syncedtool.ca/shares/" +
-                  res["id"].toString() +
-                  "/process_subscribers/",
-                {
-                  body: new URLSearchParams({
-                    login_required: 1,
-                    expires: "",
-                    download_limit: 0,
-                    download_notify: false,
-                    upload_notify: false,
-                    notify_recipients: notify_recipients, //all, new, none
-                    message: "",
-                    anon_edit: false,
-                    subscribers_json: JSON.stringify(jsonVariable),
-                  }),
-                  method: "post",
-                  headers: {
-                    "Content-Type":
-                      "application/x-www-form-urlencoded; charset=UTF-8",
-                  },
-                }
-              )
-                .then(() => {
-                  link_finished++;
-                  console.log((link_finished / link_array.length / 2) * 100);
-                  elem.style.width =
-                    (link_finished / link_array.length / 2) * 100 + "%";
-                  if ((link_finished / link_array.length / 2) * 100 == 100) {
-                    document.getElementById("alert-message").style.color =
-                      "black";
-                    document.getElementById("alert-message").innerHTML =
-                      "Finished!";
-                    console.log(jsonVariable);
-                  }
-                })
-                .catch(() => {
-                  document.getElementById("alert-message").style.color = "red";
-                  document.getElementById("alert-message").innerHTML =
-                    "Error sharing :(";
-                });
-            })
-            .catch(() => {
-              document.getElementById("alert-message").style.color = "red";
-              document.getElementById("alert-message").innerHTML =
-                "Error sharing :(";
-            });
-        }
-      })
       .catch(() => {
         document.getElementById("alert-message").style.color = "red";
         document.getElementById("alert-message").innerHTML = "Error sharing :(";
       });
-
-    document.getElementById("submit-file").style.background = "grey";
-    document.getElementById("submit-file").disabled = true;
-  });
-  function validateEmail(email) {
-    const re =
-      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-    return re.test(String(email).toLowerCase());
-  }
-  async function fetchWithTimeout(resource, options = {}) {
-    const { timeout = 10000 } = options;
-
-    const controller = new AbortController();
-    const id = setTimeout(() => controller.abort(), timeout);
-
-    const response = await fetch(resource, {
-      ...options,
-      signal: controller.signal,
-    });
-    clearTimeout(id);
-
     return response;
   }
 });
